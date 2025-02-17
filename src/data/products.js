88b0574ea70f55ff7a2ef333dc89f90a642c81e1
Ashimananda Kar <118877249+Ashimananda21@users.noreply.github.com>
2025-02-17 08:52:13 +0530

    products.js

--- src/data/products.js
+++ src/data/products.js
@@ -3,21 +3,21 @@ export const products = [
     id: 1,
     name: "BMW",
     price: 25000000,
-    image: "./images/images.jpeg",
+    image: "./images.jpeg",
     description: "Best family sportts car with 5 seats v5 model M5 mileage of 25km/l"
   },
   {
     id: 2,
     name: "ROLLS ROYCE",
     price: 100000000,
-    image: "./images/rr.jpeg",
+    image: "./rr.jpeg",
     description: "Ghost v5 2025 model with milege of 20km/l"
   },
   {
     id: 3,
     name: "Bugatti",
     price: 500000000,
-    image: "./images/bg.jpeg",
+    image: "./bg.jpeg",
     description: "Veyron 2025 model with milege of 15km/l and the fastest in the world"
   },
 
@@ -25,35 +25,35 @@ export const products = [
     id: 4,
     name: "Lamborghini",
     price: 200000000,
-    image: "./images/lam.jpeg",
+    image: "./lam.jpeg",
     description: "Aventador 2025 model with milege of 15km/l"
   },
   {
     id: 5,
     name: "Ferrari",
     price: 150000000,
-    image: "./images/fer.jpeg",
+    image: "./fer.jpeg",
     description: "F8 Tributo 2025 model with milege of 15km/l and v16 twin turbo"
   },
   {
     id: 6,
     name: "Audi",
     price: 10000000,
-    image: "./images/audi.jpeg",
+    image: "./audi.jpeg",
     description: "R8 2025 model with milege of 15km/l with v12 engine"
   },
   {
     id: 7,
     name: "Mercedes",
     price: 20000000,
-    image: "./images/merc.jpeg",
+    image: "./merc.jpeg",
     description: "Benz competition 2025 model with milege of 15km/l with v10 engine"
   },
   {
     id: 8,
     name: "Jaguar",
     price: 15000000,
-    image: "./images/jag.jpeg",
+    image: "./jag.jpeg",
     description: "F type 2025 model with milege of 15km/l with v8 engine and 4 seats"
   }
 
